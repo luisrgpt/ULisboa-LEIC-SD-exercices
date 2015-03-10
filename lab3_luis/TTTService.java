0f99a7c2c2f0929a744db8a7794a5a19fde60571
lribeirogomes <luis.ribeiro.gomes@tecnico.ulisboa.pt>
2015-03-10 13:35:32 +0000

    organized folders by labs

--- lab3_luis/TTTService.java
+++
@@ -1,8 +0,0 @@
-import ttt;
-import java.rmi.*;
-
-public interface TTTService extends Remote {
-	String currentBoard() throws RemoteException;
-	boolean play(int row, int column, int player) throws RemoteException;
-	int checkWinner() throws RemoteException;
-}
--- lab3_luis/ttt-rmi-client/src/main/java/ttt/Client.java
+++
@@ -1,9 +0,0 @@
-package ttt;
-
-public class Client {
-
-    public static void main(String[] args) throws Exception {
-    	  /* TO DO */
-    }
-
-}
